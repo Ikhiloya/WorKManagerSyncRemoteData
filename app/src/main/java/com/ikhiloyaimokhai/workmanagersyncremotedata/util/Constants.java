0160a7f947d5320eed00e3ec329cc4832736702e
Ikhiloya Imokhai <iimokhai@Ikhiloyas-MacBook-Pro.local>
2019-09-15 16:05:29 +0100

    removed redundant constants

--- app/src/main/java/com/ikhiloyaimokhai/workmanagersyncremotedata/util/Constants.java
+++ app/src/main/java/com/ikhiloyaimokhai/workmanagersyncremotedata/util/Constants.java
@@ -27,22 +27,19 @@ public final class Constants {
     public static String VERBOSE_NOTIFICATION_CHANNEL_DESCRIPTION =
             "Shows notifications whenever work starts";
     public static final CharSequence NOTIFICATION_TITLE = "WorkRequest Starting";
-    public static final String CHANNEL_ID = "VERBOSE_NOTIFICATION" ;
+    public static final String CHANNEL_ID = "VERBOSE_NOTIFICATION";
     public static final int NOTIFICATION_ID = 1;
 
-    // The name of the image manipulation work
-    static final String IMAGE_MANIPULATION_WORK_NAME = "image_manipulation_work";
+    // The name of the Sync Data work
+    public static final String SYNC_DATA_WORK_NAME = "sync_data_work_name";
+
 
     // Other keys
-    public static final String OUTPUT_PATH = "blur_filter_outputs";
-    public static final String KEY_IMAGE_URI = "KEY_IMAGE_URI";
-    static final String TAG_OUTPUT = "OUTPUT";
-
     public static final long DELAY_TIME_MILLIS = 3000;
 
-    public static final String SYNC_DATA_WORK_NAME ="sync_data_work_name";
-    public static final String TAG_SYNC_DATA ="TAG_SYNC_DATA";
+    public static final String TAG_SYNC_DATA = "TAG_SYNC_DATA";
 
     // Ensures this class is never instantiated
-    private Constants() {}
+    private Constants() {
+    }
 }
